07437c3fb41aab83873257e4b00c6abba478df79
Joshua Duke <dukejoshuae@gmail.com>
2021-11-19 16:36:42 -0500

    Switch to TMDB api for better quality images for coming soon and in theatres, populate in theatres page with all mvoies available in pages

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 const ejs = require('ejs');
 const app = express();
 const apiKey = "k_7893g9qe";
+const apikeyTMDB = '16c3a911dbc1dfc97fc092d38dba2b03';
 // const moviePoster = require(__dirname + '/public/javascript/getPosters.js')
 // let data = moviePoster();
 
@@ -16,12 +17,13 @@ app.use(express.static('public'))
 // app.use('/css', express.static(__dirname + '/node_modules/@glidejs/glide/dist/css'))
 app.use('/dist', express.static(__dirname + '/node_modules/@glidejs/glide/dist/'))
 
+
 app.get('/', (req, res)=>{
     
     /* WORKING API CALL FOR LOW QUALITY POSTERS */
 
-    const comingSoon = axios.get(`https://imdb-api.com/en/API/ComingSoon/${apiKey}`);
-    const inTheatres = axios.get(`https://imdb-api.com/en/API/InTheaters/${apiKey}`);
+    const comingSoon = axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${apikeyTMDB}&language=en-US&page=1&region=US`);
+    const inTheatres = axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apikeyTMDB}&language=en-US&page=1`);
 
     axios.all([comingSoon, inTheatres])
         .then(
@@ -31,7 +33,7 @@ app.get('/', (req, res)=>{
 
                 console.log(comingSoonData.data, inTheatresData.data);
 
-                res.render('home', {comingSoon : comingSoonData.data.items, theatres: inTheatresData.data.items})
+                res.render('home', {comingSoon : comingSoonData.data.results, theatres: inTheatresData.data.results})
             })
         )
         .catch(errors => {
@@ -39,34 +41,168 @@ app.get('/', (req, res)=>{
         })
 })
 
+// app.get('/', (req, res)=>{
+    
+//     /* WORKING API CALL FOR LOW QUALITY POSTERS */
+
+//     const comingSoon = axios.get(`https://imdb-api.com/en/API/ComingSoon/${apiKey}`);
+//     const inTheatres = axios.get(`https://imdb-api.com/en/API/InTheaters/${apiKey}`);
+
+//     axios.all([comingSoon, inTheatres])
+//         .then(
+//             axios.spread((...responses) => {
+//                 const comingSoonData = responses[0];
+//                 const inTheatresData = responses[1];
+
+//                 console.log(comingSoonData.data, inTheatresData.data);
+
+//                 res.render('home', {comingSoon : comingSoonData.data.items, theatres: inTheatresData.data.items})
+//             })
+//         )
+//         .catch(errors => {
+//             console.error(errors)
+//         })
+// })
+
 app.get('/home/:option', (req, res)=> {
     let pageOption = req.params.option;
 
     if(pageOption == 'comingSoon'){
     let title = 'Coming Soon';
-    const url = `https://imdb-api.com/en/API/ComingSoon/${apiKey}`;
+    const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apikeyTMDB}&language=en-US&region=US`;
 
     axios.get(url)
         .then((response)=>{
             let data = response.data;
             console.log('Coming Soon:');
             console.log(data);
-            res.render('homeOptions', {data: data.items, title: title})
+            res.render('homeOptions', {data: data.results, title: title})
         })
         .catch(err => console.error(err));
 
     } else if(pageOption == 'inTheatres') {
         let title = 'In Theatres';
-        const url = `https://imdb-api.com/en/API/InTheaters/${apiKey}`;
+        const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apikeyTMDB}&language=en-US&region=US`;
+    
+        // let totalPages = data;
 
-        axios.get(url)
-            .then((response)=>{
-                let data = response.data;
-                console.log('In Theatres:');
-                console.log(data);
-                res.render('homeOptions', {data: data.items, title:title})
-            })
-            .catch(err => console.error(err));
+        // axios.get(url)
+        //     .then((response)=>{
+        //         let data = response.data;
+        //         console.log('In Theatres:');
+
+        //         totalPages = data.totalPages;
+
+        //         // for (let i = 0; index < totalPages; index++) {
+               
+                    
+        //         // }
+        //         res.render('homeOptions', {data: data.results, title:title})
+        //     })
+        //     .catch(err => console.error(err));
+
+            // console.log(totalPages);
+
+            async function getUser() {
+                try {
+                  const response = await axios.get(url);
+                  let data = response.data;
+                  console.log(data.total_pages)
+                  return data.total_pages;
+                } catch (error) {
+                  console.error(error);
+                }
+              }
+
+            getUser()
+                .then(data =>{
+                    console.log('inside get user');
+                    console.log(data);
+                    return getPageData(data)
+                    // async function getPageData() {
+                    //     let pagesData = [];
+
+                    //     for (let index = 1; index < data; index++) {
+                    //         try {
+                    //             const response = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apikeyTMDB}&language=en-US&page=${index}region=US`);
+      
+                    //             let data = response.data;
+                    //             pagesData.push(data);
+                    //             // console.log(data.total_pages)
+                    //             // return data.total_pages;
+                    //           } catch (error) {
+                    //             console.error(error);
+                    //           }
+                    //     }
+                    //     return pagesData;
+                    //   }   
+
+
+                })  
+                .catch(err => console.log(err))
+
+                async function getPageData(data) {
+                    let pagesDataArr = [];
+
+                    for (let i = 0; i < 3; i++) {
+                        let pageData = axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apikeyTMDB}&language=en-US&page=${i}region=US`);
+                        pagesDataArr[i] = pageData;
+                        
+                    }
+
+
+                    axios.all(pagesDataArr)
+                    .then(
+                        axios.spread((...responses) => {
+                            let data = [];
+                            responses.forEach(element => {
+                                // console.log(element.data)
+                                data.push(element.data.results);
+                            });
+
+                            // console.log(data[0])
+                            // console.log(typeof data)
+                            // const comingSoonData = responses[0];
+                            // const inTheatresData = responses[1];
+            
+                            // console.log(comingSoonData.data, inTheatresData.data);
+            
+                            res.render('homeOptions', {data: data, title:title})
+                            //     })
+                        })
+                    )
+                    .catch(errors => {
+                        console.error(errors)
+                    })
+
+                    // try {
+                    //     console.log('Inside get page data function')
+                    //     console.log(data);
+                    //   return data;
+                    // } catch (error) {
+                    //   console.error(error);
+                    // }
+                  }
+
+            //for each object store in array
+            // getPageData()
+            //     .then(data => {
+            //         console.log('Inside get page data')
+            //         console.log(data);
+            //     })
+            //     .catch(err => console.log(err))
+
+
+            //   console.log(totalPages);
+
+        // axios.get(url)
+        //     .then((response)=>{
+        //         let data = response.data;
+        //         console.log('In Theatres:');
+        //         console.log(data.total_pages);
+        //         res.render('homeOptions', {data: data.results, title:title})
+        //     })
+        //     .catch(err => console.error(err));
     }
 
     // res.render('homeOptions', {title : title});
@@ -107,8 +243,22 @@ app.get('/watched', (req, res)=>{
     res.render('watched')
 });
 
+app.get('/addNew', (req, res)=>{
+    res.render()
+})
+
 
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log('Server has started...')
 })
+
+/*
+    Call tmdb api for coming soon and in theatres
+    once clicked on an option
+    call the external ids api to retrieve IMDB api
+
+    use imdb api to retrieve movie details for the selected
+    movie page
+
+*/
