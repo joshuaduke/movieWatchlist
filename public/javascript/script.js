3d23c8e2098d347347055caa304ea44e1b5a5f75
Joshua Duke <dukejoshuae@gmail.com>
2021-12-26 18:03:07 -0500

    Service worker update changed to relative path

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,9 +1,13 @@
 if("serviceWorker" in navigator){
-    navigator.serviceWorker.register("service-worker.js").then(registration =>{
+    navigator.serviceWorker.register("./service-worker.js").then(registration =>{
         console.log("SW Registered! ");
         console.log(registration);
     }).catch(err => {
         console.log("SW registration failed");
         console.log(err);
+        console.log(window.location.href);
+        var script = document.currentScript;
+        var fullUrl = script.src;
+        console.log(fullUrl);
     })
 }
