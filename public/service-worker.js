0082ceae641cbe0698060461df8524b290bb3fff
Joshua Duke <dukejoshuae@gmail.com>
2021-12-29 04:31:53 -0500

    Testing fetch changes for service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,20 +1,32 @@
+const assets = [
+    '/',
+    '/css',
+    '/images/Image-Not-Available.png',
+    '/images/tmdbLogo.svg',
+    '/images/darkKnightTextLess.jpeg',
+    '/public/javascript/script.js'
+]
+
+//install service worker
 self.addEventListener("install", e =>{
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./"]);
+            return cache.addAll(assets);
         })
     );
 })
 
+//activate
 self.addEventListener('activate', e =>{
     console.log('Service worker is activated');
 })
 
+//fetch
 self.addEventListener("fetch", e => {
     console.log("Intercepting fetch request for " + e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
-        })
-    );
+    // e.respondWith(
+    //     caches.match(e.request).then(response => {
+    //         return response || fetch(e.request);
+    //     })
+    // );
 }) 
